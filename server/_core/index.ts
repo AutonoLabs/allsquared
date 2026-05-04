2b2cc5e26517b8902515a47ab8f0590dfa94f7fc
Eli Bernstein (AutonoLabs) <eli@autonolabs.ai>
2026-05-04 15:24:37 +0300

    Allow Clerk worker CSP

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -44,6 +44,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.google-analytics.com https://clerk.allsquared.io https://accounts.allsquared.io https://*.clerk.accounts.dev",
+      "worker-src 'self' blob:",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https: blob:",
